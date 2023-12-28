ce7767b2f84f889df06cc533829c3e16c662ccd2
Abhay Dedkawala <abhay.lynxsoftech@gmail.com>
2023-12-28 23:43:39 +0530

    Add Users Modal Add From Position & To Position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+var toX, toY;
+$(document).ready(function () {
+    $("#userModal").modal("show");
+})
+
+$("#userModal input[type=radio]").change(function () {
+    localStorage.setItem("user", this.value);
+    $("#userModal").modal("hide");
+})
+
 $("div.card").click(function (event) {
-    console.log(event);
+    toX = this.children[0].x;
+    toY = this.children[0].y;
+
     $("#iconsModal").modal("show");
-    $("#iconsModal .modal-dialog").css({ top: event.screenY, left: event.screenX });
+    $("#iconsModal .modal-dialog").css({ top: event.clientY, left: event.clientX });
 })
 
 $("#iconsModal img").click(function () {
-    this.style.animation = "sprite 1.5s steps(36) 1";
+    const fromLeft = $("div[data-user='" + localStorage.getItem("user") + "']")[0].offsetLeft;
+    const fromTop = $("div[data-user='" + localStorage.getItem("user") + "']")[0].offsetTop;
+    const htmlEmoji = `<img style="background:url('${this.dataset.image}') 0 0 no-repeat; position:absolute;top:${fromTop}px;left:${fromLeft}px;z-index:10" height=176 width=176 />`;
+    $("#emoji").html(htmlEmoji);
+
+    $("#iconsModal").modal("hide");
+    // this.style.animation = "sprite-image 1.5s steps(36) 1";
 })
