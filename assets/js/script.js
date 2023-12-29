b311e0ace9b4e4a64c52550354d2c2376effd89c
Abhay Dedkawala <abhay.lynxsoftech@gmail.com>
2023-12-29 17:51:26 +0530

    Modify Code for Stop.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 $("#userModal input[type=radio]").change(function () {
     localStorage.setItem("user", this.value);
     $("#userModal").modal("hide");
+    $(`div[data-user="${this.value}"] img`).css({"border": "2px solid var(--bs-primary)"});
 })
 
 $("div.card").click(function (event) {
@@ -27,14 +28,15 @@ $("div.card").click(function (event) {
 $("#iconsModal img").click(function () {
     $("#iconsModal").modal("hide");
     const toUser = to, toUserX = toX, toUserY = toY;
-    const animationDuration = 8;
+    const animationDuration = 2;
     const fromLeft = $("div[data-user='" + localStorage.getItem("user") + "']")[0].offsetLeft;
     const fromTop = $("div[data-user='" + localStorage.getItem("user") + "']")[0].offsetTop;
-    const htmlEmoji = `<div id="${toUser}"><img src="${this.src}" style="object-position: 0 0;object-fit:cover;position:absolute;top:${fromTop}px;left:${fromLeft}px;z-index:10;animation:sprite-image-${toUser} ${animationDuration}s steps(${this.dataset.frames}) 1;animation-fill-mode:forwards" height=${this.height} width=${this.width} />`;
-    const spriteImageStyle = `<style>@keyframes sprite-image-${toUser}{to {object-position: ${this.dataset.framesLayout == "horizontal" ? "100% 0" : "0 100%"};top:${toUserY}px;left:${toUserX}px;}}</style></div>`;
-    $("#emoji").append(htmlEmoji + spriteImageStyle);
+    const htmlEmoji = `<div id="${toUser}"><img src="${this.src}" style="object-position: 0 0;object-fit:cover;position:absolute;top:${fromTop}px;left:${fromLeft}px;z-index:10;animation-name:sprite-user-${toUser},sprite-image-${toUser};animation-delay:0s,${animationDuration}s;animation-duration:${animationDuration}s,${animationDuration}s;animation-timing-function:linear,steps(${this.dataset.frames});animation-iteration-count:1,1;animation-fill-mode:forwards,forwards" height=${this.height} width=${this.width} />`;
+    const spriteImageStyle = `<style>@keyframes sprite-image-${toUser}{to {object-position: ${this.dataset.framesLayout == "horizontal" ? "100% 0" : "0 100%"}}} `;
+    const spriteUserStyle = `@keyframes sprite-user-${toUser}{to {top:${toUserY}px;left:${toUserX}px;}}</style></div>`;
+    $("#emoji").append(htmlEmoji + spriteImageStyle + spriteUserStyle);
 
     setTimeout(function () {
         $(`#emoji #${toUser}`).remove();
-    }, animationDuration * 1000)
+    }, animationDuration * 2000)
 })
